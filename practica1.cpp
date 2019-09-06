46ffba7e3d94f089c9ac3efd6c4b395736edee9e
Laboratorio FISI <LaboratorioFISI@UNMSM.pe>
2019-09-06 12:31:15 -0500

    first commit

---
+++ practica1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int a,b,r;
+int main(){
+    cout<<"Ingrese dos numeros"<<endl;
+    cin>>a;
+    cin>>b;
+    r=a+b;
+    cout<<"La suma es: "<<r<<endl;
+    return 0;
+}
+
